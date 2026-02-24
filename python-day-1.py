163709fd09a928e2cc5ee179a369cb64a2c2e7de
mohdarshal <mohdarshal10@gmail.com>
2026-02-24 22:28:55 +0530

    Add files via upload

---
+++ python-day-1.py
@@ -0,0 +1,27 @@
+#print("Hello, I am learing Python for  Devops ")
+
+
+#name=" My Nameis Mohamed Arshal K"
+#college=" And styding at Saveetha medical college "
+#print(name,college)
+
+
+# name=input("enter your name :")
+# print("welcome",name)
+
+# name=input("enter your name :")
+# fav_tool=input("enter your fav tool docker, kubernetes, ansible :")
+# print(name,"loves",fav_tool)
+
+
+# age=int(input("enter your age :"))
+# a5=age+5
+# print("after 5 years your age will be:",a5)
+
+# role =  input("enter your role:")
+# if role == "devops":
+    # print("welcome engineer")
+# else:
+    # print("keep learing!") 
+# print(role)
+
