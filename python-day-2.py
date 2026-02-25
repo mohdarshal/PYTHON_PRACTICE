9fb2c3ce3b2f0fa032e38a8bfe31b4f1afb21ee8
Mohd Arshal <mohdarshal10@gmail.com>
2026-02-25 16:40:02 +0530

    update code

---
+++ python-day-2.py
@@ -0,0 +1,44 @@
+# --- program to add two string---
+
+# Name=input("enter your name :")
+# School=input("enter your school name:")
+# add=Name+School
+# print(add)
+
+
+#---program to change to upper case--- 
+
+# Name=input("enter your name:")
+# print(Name.upper())
+
+
+# ---- program to find the length of the string--
+
+# Name=input("enter your name to find  the length :")
+# print("your name has",len(Name),"character")
+
+
+# --- program to create Greeting Message---
+
+# Role = input("enter your role :")
+# print("WELCOME",Role.upper(),"ENGINEER")
+
+
+# --- program to create a user name ----
+# First_name=input("enter your  first name:")
+# Birth_year=int(input("enter your birth year :"))
+# print(First_name+str(Birth_year))
+
+
+
+# ---program  to find short or long name ---
+# name=input("enter your name :")
+# if len(name)>5:
+    # print("long name")
+# else:
+    # print("short name")
+
+# ----program to project name upper case---
+
+# project=input("enter your project name:")
+# print("DEPLOYING",project.upper())
