2d72024106697e70f099bc273bfed86dbc0bce15
mohdarshal <mohdarshal10@gmail.com>
2026-02-26 22:15:42 +0530

    Add files via upload

---
+++ python-day-3.py
@@ -0,0 +1,27 @@
+# ---Write a program using a for loop---
+# for i in range(3):
+    # print("hello devops")
+
+# ---write a program to print  0 1 2 3 4 ---
+# for i in range(4):
+    # print(i)
+
+
+# ---write a program to print   1 2 3 4 ---
+# for i in range(1,5):
+    #  print(i)
+
+
+# ---write a code in for loop--
+# a="step"
+# for i in range(3):
+    # print(a,i)
+
+# ---write a program to print   2 3 4 5 6 ---
+# for i in range(2,7):
+    # print(i)
+
+# ---write a program to print  Repeat With Number---
+# a="run"
+# for i in range(5):
+    # print(a,i)
