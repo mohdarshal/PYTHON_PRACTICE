3a41955c63aaca931adc8680ead7b629b60d3b94
mohdarshal <mohdarshal10@gmail.com>
2026-02-27 16:04:04 +0530

    Add files via upload

---
+++ python-day-4.py
@@ -0,0 +1,36 @@
+# ---Question 1 — Simple Condition----
+
+# num=int(input("enter a number :"))
+# if  num>10:
+    # print("big number:")
+# else:
+    # print("small number")
+
+
+
+# ----Question 2 — Role Checker----
+
+# role=input("enter your role :")
+# if role=="devops":
+    # print("WELCOME DEVOPS ENGINEER")
+# else:
+    # print("invalid role")
+
+
+# ---Question 3 — Positive or Negative--
+# num=int(input("enter your number:"))
+# if num>0:
+    # print("positive number:")
+# else:
+    # print("negative number")
+
+# ---Question 4 — Grade System (elif)---
+# score=int(input("enter the score :"))
+# if score>=90:
+    # print("GRADE A")
+# elif score<=60:
+    # print("GRADE B")
+# else:
+    # print("GRADE C")
+
+
