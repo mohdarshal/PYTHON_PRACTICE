91f0e14480d4610da00c5590a5990a9ad645d341
mohdarshal <mohdarshal10@gmail.com>
2026-02-28 14:08:33 +0530

    Add files via upload

---
+++ python-day-5.py
@@ -0,0 +1,51 @@
+
+# ---- age plus 1 calling function ---
+# def say_age(age):
+    # print("you will be", age+1, "years old next year")
+# say_age(18)
+
+# --- to call a function---
+
+# def hello():
+    # print("hello engineer")
+# hello()
+
+# ----greet a user----
+
+# def greet(name):
+    # print("hello",name)
+# greet("arshal")
+
+
+# --- add two number using function ---
+# def add(a,b):
+    # print(a+b)
+# add(5,3)
+
+# ---to check even or odd---
+# def num(n):
+    # if(n%2==0):
+        # print("even")
+    # else:
+        # print("odd")
+# num(2)
+
+
+
+
+# ---- mini project ------
+# def get_user():
+    # name= input("what is your name?: ")
+    # return name
+# def get_role():
+    # role= input("what is your role?: ")
+    # return role
+# name= get_user()
+# role =get_role()
+# if role =="devops":
+    # print("full access")
+# elif role=="mlops":
+    # print("limited access")
+# else:
+    # print("guest access")
+
