d100c004732f02e6c8b73bea38770e47e1ebbef6
mohdarshal <mohdarshal10@gmail.com>
2026-03-02 21:18:53 +0530

    Add files via upload

---
+++ python-day-6.py
@@ -0,0 +1,38 @@
+# Question 1 — Create & Access
+
+# a=[10,20,30]
+# print(a[1])
+
+
+
+# Question 2 — Update Value
+# num=[10,20,30]
+# num[1]=40
+# print(num)
+
+
+# Question 3 — Add Item
+# num=[10,20,30,40]
+# a=[70,67]
+# num.extend(a)
+# print(num)    
+
+
+# Question 4 — Remove Item
+# num=[10,20,30,40]
+# num.pop(2)
+# print(num)
+
+
+# Question 5 — Loop Through List
+# num=[10,20,30]
+# for i in range (len(num)):
+    # print(num[i])
+
+
+# Q6 — Check Exists
+# nums = [5,10,15,20]
+
+# if 15 in nums:
+    # print("Found")
+
